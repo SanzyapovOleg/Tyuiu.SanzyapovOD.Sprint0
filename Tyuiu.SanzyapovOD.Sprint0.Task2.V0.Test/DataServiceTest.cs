e6146de5a577d3a4b46761d55c0592f805da289f
Tyuiu.SanzyapovOD@outlook.com <oleg.sanzjapov@mail.ru>
2024-09-15 17:06:39 +0500

    Task2

---
+++ Tyuiu.SanzyapovOD.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.SanzyapovOD.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.SanzyapovOD.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Олег";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет..., Олег", res);
+
+        }
+    }
+}
---
+++ Tyuiu.SanzyapovOD.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.SanzyapovOD.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.SanzyapovOD.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Олег"));
+            Console.ReadKey();
+        }
+    }
+}
