d5a6933f15ab388cf56a748717d577c576a6dfc0
Tyuiu.SanzyapovOD@outlook.com <oleg.sanzjapov@mail.ru>
2024-09-15 17:54:40 +0500

    Task3

---
+++ Tyuiu.SanzyapovOD.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Tyuiu.SanzyapovOD.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b)
+
+        {
+            return a + b;
+        }
+    
+    }
+
+}
---
+++ Tyuiu.SanzyapovOD.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+using Tyuiu.SanzyapovOD.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.SanzyapovOD.Sprint0.Task3.V0.Test
+
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
